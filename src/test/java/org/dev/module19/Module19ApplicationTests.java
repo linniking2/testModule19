320efa232a691e45f86f49836fbcaaf9b079e393
linniking2 <38794009+linniking2@users.noreply.github.com>
2024-12-27 16:00:06 +0200

    Initial commit

---
+++ src/test/java/org/dev/module19/Module19ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dev.module19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Module19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true, "This test will always pass.");
+	}
+
+}
